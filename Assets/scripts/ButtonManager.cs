3091d5acda9491a2ee131236ea8dcfb77717b345
Diana-Mo <ovod.diana@gmail.com>
2021-08-01 20:28:05 -0700

    fixed colors, checks for target color

--- Assets/scripts/ButtonManager.cs
+++ Assets/scripts/ButtonManager.cs
@@ -31,34 +31,50 @@ public class ButtonManager : MonoBehaviour
     //{green, pink, white, blue, yellow, purple} RGB
     // Color[] colors = { new Color(0,1,0,1), new Color(1,0,0,1), new Color(1,1,1,1), new Color(0,0,1,1),  new Color(1,1,0,1), new Color(0, 0, 0, 1)};
     // Random rndColor = new Random();
-    void Start () 
+    // int randomTargetIndex = Random.Range(0, 5);
+    void Awake ()
     {
         
+        // int randomTargetIndex = Random.Range(0, 5);
+    }
+    void Start () 
+    {
         // GameObject newButton2 = Instantiate(button) as GameObject;
         // newButton.transform.SetParent(newCanvas.transform, false);
         // newButton2.transform.SetParent(canvas.transform);
 
-        Color greenClr = new Color(0.349f, 0.126f, 0, 0.011f);
-        Color pinkClr = new Color(0.349f, 0.126f, 0, 0.011f);
+        int randomTargetIndex = Random.Range(0, 5);
+        int totalPoints = 0;
+        // int randomTargetIndex = 2;
+
+        Color greenClr = new Color(0.603f, 0.917f, 0.615f, 1);
+        Color pinkClr = new Color(0.945f, 0.674f, 0.780f, 1); //
         Color whiteClr = new Color(1,1,1,1);
-        Color blueClr = new Color(0.384f, 0.085f, 0, 0.031f);
-        Color yellowClr = new Color(0, 0.052f, 0.204f, 0.019f);
-        Color purpleClr = new Color(0.264f, 0.243f, 0, 0.050f);
+        Color blueClr = new Color(0.584f, 0.749f, 0.917f, 1);
+        Color yellowClr = new Color(0.980f, 0.894f, 0.498f, 1);
+        Color purpleClr = new Color(0.741f, 0.549f, 0.929f, 1);
+        
+        // Color greenClr = new Color(0.349f, 0.126f, 0, 0.011f);
+        // Color pinkClr = new Color(0.349f, 0.126f, 0, 0.011f);
+        // Color whiteClr = new Color(1,1,1,1);
+        // Color blueClr = new Color(0.384f, 0.085f, 0, 0.031f);
+        // Color yellowClr = new Color(0, 0.052f, 0.204f, 0.019f);
+        // Color purpleClr = new Color(0.264f, 0.243f, 0, 0.050f);
 //new Color(0, 0.052f, 0.204f, 0.019f), new Color(0.264f, 0.243f, 0, 0.050f)
 
         for (int i = 0; i < MAX_BTNS; i++) 
         {
             var buttonObj = Instantiate(buttonPrefab);
             var button = buttonObj.GetComponent<Button>();
+            buttonObj.transform.SetParent(canvas.transform);
             var image = buttonObj.GetComponent<Image>();
             Color[] colors = {greenClr, pinkClr, whiteClr, blueClr,  yellowClr, purpleClr};
-            var randomIndex = Random.Range(0, colors.Length);
-            image.color = colors[randomIndex];
-            buttonObj.transform.SetParent(canvas.transform);
+            var randomClrIndex = Random.Range(0, colors.Length);
+            image.color = colors[randomClrIndex];
             int buttonIndex = i;
             buttonObj.name = buttonIndex.ToString();
-            // buttonObj.
-            button.onClick.AddListener(() => onButtonClicked(button, buttonIndex, randomIndex));
+            button.onClick.AddListener(() => onButtonClicked(button, buttonIndex, randomClrIndex, randomTargetIndex, totalPoints));
+            // totalPoints += onButtonClicked(button, buttonIndex, randomClrIndex, randomTargetIndex, totalPoints);
         }
 
         // Color btnColor = GetComponent<Button> ().Color;
@@ -66,9 +82,16 @@ public class ButtonManager : MonoBehaviour
         // GetComponent<Button> ().btnColor = btnColor;
     }
 
-    void onButtonClicked(Button btn, int index, int clrIndex) {
+    void onButtonClicked(Button btn, int index, int clrIndex, int targetIndex, int points) {
         Debug.Log("clicked: " + btn.name);
         Debug.Log("color number: " + clrIndex);
+        Debug.Log("target color: " + targetIndex);
+        if (clrIndex == targetIndex)
+        {
+            points += 5;
+        }
+        Debug.Log("score: " + points);
+        // return points;
     }
 
     // int color;
